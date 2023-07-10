45394a90dcc8623cf065375bbb4d521f8ed90cdd
Eldeniz08 <eldenizpaulo2016@gmail.com>
2023-07-10 16:05:10 -0300

    maisumpasso

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
-var map;
+const valorPesq = document.getElementById("valorpesq")
+const id_adress = document.getElementById("id_adress")
+const id_location = document.getElementById("id_location")
+const id_timezone = document.getElementById("id_timezone")
+const id_isp = document.getElementById("id_isp")
+const btn_pesquisar = document.getElementById("btn_pesquisar")
+let map;
+
+
+
+btn_pesquisar.addEventListener("click", ()=>{
+    let valueinput = valorPesq.value
+    const endPoint = 'https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_59SEiIwsCwFBEyGE7fQPYEL28z8Ou&ipAddress=' + valueinput
+    fetch(endPoint)
+    .then(res =>res.json())
+    .then(dados=>{
+        console.log(dados)
+        id_adress.innerHTML = dados.ip
+        id_location.innerHTML = dados.location.country + ", " + dados.location.region
+        id_timezone.innerHTML = dados.location.timezone
+        id_isp.innerHTML = dados.isp
+        console.log(dados.location.lat,dados.location.lng)
+        map.remove()
+        map = L.map('map').setView([-7.32861, -35.3325], 22)
+       })
+        
+       
+})
+
 
 function sucess(pos){
     console.log(pos.coords.latitude, pos.coords.longitude)
-
-    if(map === undefined){
-       map = L.map('map').setView([pos.coords.latitude, pos.coords.longitude], 22);
-    }else{
-        map.remove()
-        map = L.map('map').setView([pos.coords.latitude, pos.coords.longitude], 22);
-    }
-
-
+        
+    map = L.map('map').setView([pos.coords.latitude, pos.coords.longitude], 22);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
@@ -19,7 +40,7 @@ L.marker([pos.coords.latitude, pos.coords.longitude]).addTo(map)
     .bindPopup('Você esta aqui.')
     .openPopup();
 }
-
+    
 
 function error(err){
     console.log(err)
@@ -29,4 +50,3 @@ var watchID = navigator.geolocation.watchPosition(sucess, error, {
     enableHighAccuracy: true,
     timeout: 5000
 })
-
